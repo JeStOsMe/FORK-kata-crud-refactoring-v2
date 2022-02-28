03e047f35795bf55ca5ad0d5ed565ee0f50d1204
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-02-27 22:31:51 -0500

    list/ListView terminado

---
+++ front/src/list/ListView.js
@@ -0,0 +1,49 @@
+import React, {useState, useContext, useEffect} from "react";
+import ToDoForm from "../todo/Formview";
+import ToDoList from "../todo/ListView";
+import consumer from "./consumer";
+import events from "./events";
+import Store from "../Store";
+
+export default () => {
+    const { state: {list, todo}, dispatch } = useContext(Store);
+    const [isLoaded, setLoaded] = useState(false);
+
+    useEffect( () => {
+        consumer.findAll().then((response) => {
+            if(response.ok){
+                response.json().then((list) => {
+                    dispatch(events.finded(list));
+                    console.log("successful list");
+                });
+            }
+            setLoaded(true);
+        })
+    }, [dispatch]);
+
+    const onDelete = (listId) => {
+        consumer.delete(listId).then((response) => {
+            if (response.ok) {
+                dispatch(events.deleted(listId));
+            }
+        })
+    };
+
+    return <div>
+        {!isLoaded && <div>Loading...</div>}
+        {list.elements.length === 0 && <div>empty list </div>}
+        {list.elements.map((element)=> {
+            return <div key={element.id} id={"list-to-do-" + element.id}> 
+                        <fieldset>
+                            <legend>
+                                {element.name.toUpperCase()}
+                                <button onClick={() => onDelete(element.id)}>Eliminar</button>
+                            </legend>
+                            <ToDoForm listId={element.id} todo={todo}></ToDoForm>
+                            <ToDoList listId={element.id} todo={todo}></ToDoList>
+                        </fieldset>
+
+            </div>
+        })}
+    </div>
+}
